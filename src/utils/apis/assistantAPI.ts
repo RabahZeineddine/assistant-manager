da8b00ef378712aeb5cd673d7a35ef6776ff2d73
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2021-01-05 12:43:34 -0300

    timeout post request changed

--- src/utils/apis/assistantAPI.ts
+++ src/utils/apis/assistantAPI.ts
@@ -9,6 +9,6 @@ export default class AssistantAPI {
     public static async testSkill(file: File) {
         const form = new FormData()
         form.append('file', file)
-        return HttpRequest.post(env.API.URL, form, { rawResponse: true, responseType: 'arraybuffer' })
+        return HttpRequest.post(env.API.URL, form, { rawResponse: true, responseType: 'arraybuffer', timeout: 99999999 })
     }
 }
